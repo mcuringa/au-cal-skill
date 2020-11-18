b18fc525a9339e3e01066dfb6002a90870757e19
mxc <matt@curinga.com>
2020-11-18 16:35:58 -0500

    version from demo video

--- __init__.py
+++ __init__.py
@@ -2,76 +2,44 @@ from mycroft import MycroftSkill, intent_file_handler, util
 
 from lingua_franca.format import nice_date, nice_date_time, nice_time
 from datetime import date
-import os, sys
-import json
-# import data
+
+# load the vents property from data.py
+from .data import events
 
 class AdelphiCalendar(MycroftSkill):
+
     def __init__(self):
         MycroftSkill.__init__(self)
-        self.ay = ""
-        self.current_term = ""
-        self.spring_dates = {}
-        self.fall_dates = {}
+
+
+    @intent_file_handler('springbreak.intent')
+    def handle_springbreak(self, message):
+        self.speak_dialog("springbreak")
 
 
 
+    @intent_file_handler('semester.start.intent')
+    def handle_semester_start(self, message):
 
-    def load_data(self, filename):
-        # dir = os.getcwd()
-        # pathname = os.path.dirname(sys.argv[0])
-        # dir = os.path.abspath(pathname)
-        dir = util.resolve_resource_file("skills/adelphi-calendar-skill/" + filename)
-        # self.log.info(f"{dir}")
-        with open(dir) as jf:
-            data = json.load(jf)
-        return data
-
-    def initialize(self):
-
-
-        def clean(jso):
-            o = jso.copy()
-            o["no-scool"] = bool(o["no-school"])
-            o["start"] = date.fromiso(o["start"])
-            if o["end"]:
-                o["end"] = date.fromiso(o["end"])
-            return o
-
-        self.fall_dates = self.load_data("fall.json")
-        self.log.info(self.fall_dates["summer-graduation"]["start"])
-
+        # first figure out if they want to know about the fall or spring semester
         month = date.today().month
-        if month < 6:
-            self.current_term = "spring"
-        else:
-            self.current_term = "fall"
+        term = "spring"
+        if month > 5 and month < 10:
+            term = "fall"
 
+        if "term" in message.data:
+            term = message.data["term"]
 
+        self.log.info(f"asking about the start of the {term} semester")
 
+        # get the data from out dicts
+        key = "first-day-" + term
+        event = events[key]
+        event["start"] = nice_date(date.fromisoformat(event["start"]))
 
+        # send the data to the dialog
 
-
-
-    @intent_file_handler('semester.adelphi.intent')
-    def handle_calendar_adelphi(self, message):
-        # self.log.info("handling semester intent")
-        term = message.data.get("term", self.current_term)
-        self.log.info(f"asking about the {term} semester")
-        self.speak("hold on")
-        # if term == "spring":
-        #     self.speak_dialog("spring.term.adelphi")
-        # else:
-        #     self.speak_dialog("fall.term.adelphi")
-
-
-    # @intent_file_handler('calendar.adelphi.intent')
-    # def handle_calendar_adelphi(self, message):
-    #     # self.speak_dialog('calendar.adelphi')
-    #     event = message.data["event"]
-    #     print("calendar event", event)
-    #     self.speak("you want to know about " + str(event))
-
+        self.speak_dialog("event", event)
 
 
 
--- data.py
+++ data.py
@@ -1,61 +1,45 @@
-from datetime import date
-fall = {
+
+events = {
+  # fall events
   "summer-graduation": {
     "event": "Official Summer Graduation Date",
-    "start": date.fromisoformat("2020-08-31"),
+    "start": "2020-08-31",
     "end": None
   },
   "first-day-fall": {
     "event": "First Day of Classes",
-    "start": date.fromisoformat("2020-08-31"),
+    "start": "2020-08-31",
     "end": None
   },
   "thanksgiving-break": {
-    "event": "Thanksgiving Break - No Classes",
-    "start": date.fromisoformat("2020-11-25"),
-    "end": date.fromisoformat("2020-11-29")
+    "event": "Thanksgiving Break",
+    "start": "2020-11-25",
+    "end": "2020-11-29"
   },
-  "finals-week": {
+  "finals-week-fall": {
     "event": "Online/remote final exam period",
-    "start": date.fromisoformat("2020-12-15"),
-    "end": date.fromisoformat("2020-12-21")
-  }
-}
-
-spring = {
+    "start": "2020-12-15",
+    "end": "2020-12-21"
+  },
+  # spring events
   "first-day-winter": {
     "event": "First Day of Classes - Intersession",
-    "start": date.fromisoformat("2020-01-04"),
+    "start": "2021-01-04",
     "end": None
   },
   "first-day-spring": {
-    "event": "First Day of Classes - Spring",
-    "start": date.fromisoformat("2020-01-26"),
+    "event": "First Day of Classes - Spring Semester",
+    "start": "2021-01-26",
     "end": None
   },
-  "summer-registration": {
-    "event": "Registration begins for Summer 21",
-    "start": date.fromisoformat("2020-03-22"),
-    "end": None
-  },
-  "grad-registration": {
-    "event": "Graduate registration begins for Fall 21",
-    "start": date.fromisoformat("2020-03-31"),
-    "end": None
-  },
-  "undergrad-registration": {
-    "event": "Undergraduate registration begins for Fall 21",
-    "start": date.fromisoformat("2020-04-05"),
-    "end": None
-  },
-  "finals-week": {
+  "finals-week-spring": {
     "event": "Final Exam period",
-    "start": date.fromisoformat("2020-05-14"),
-    "end": date.fromisoformat("2020-05-20")
+    "start": "2021-05-14",
+    "end": "2021-05-20"
   },
   "graduation": {
     "event": "Commencement Ceremony",
-    "start": date.fromisoformat("2020-05-24"),
+    "start": "2021-05-24",
     "end": None
   }
 }
